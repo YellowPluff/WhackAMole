b82e9152211bed1ce55f251b1f442cdfd24ec412
Andy Ruben <android@google.com>
2020-02-14 11:54:19 -0500

    first commit

--- app/src/main/java/com/example/whackamole/MainActivity.java
+++ app/src/main/java/com/example/whackamole/MainActivity.java
@@ -4,15 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import androidx.gridlayout.widget.GridLayout;
 import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private GridLayout grid;
     private Drawable moleImage;
     private ImageView[] imageViews;
+    private int moleLocation;
+    private Random random;
+
+    private Handler handler;
+    private MoleHopper moleHopper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +30,46 @@ public class MainActivity extends AppCompatActivity {
         grid = findViewById(R.id.gridLayout);
         moleImage = getDrawable(R.drawable.mole);
         imageViews = new ImageView[16];
+        random = new Random();
+        moleHopper = new MoleHopper();
+        moleLocation = random.nextInt(16);
+        handler = new Handler();
         for(int i=0; i<16; i++) {
             imageViews[i] = (ImageView) getLayoutInflater().inflate(R.layout.mole_view, null);
-            imageViews[i].setImageDrawable(moleImage);
+            imageViews[i].setMinimumWidth(270);
+            imageViews[i].setMinimumHeight(270);
+            if(i == moleLocation) {
+                imageViews[i].setImageDrawable(moleImage);
+            }
             grid.addView(imageViews[i]);
         }
     }
 
     public void startPressed(View v) {
+        if(handler.hasCallbacks(moleHopper))
+        {
+            handler.removeCallbacks(moleHopper);
+        } else {
+            handler.postDelayed(moleHopper, 0);
+        }
+    }
 
+    private class MoleHopper implements Runnable {
+        @Override
+        public void run() {
+            imageViews[moleLocation].setImageDrawable(null);
+            imageViews[moleLocation].setOnClickListener(null);
+            moleLocation = random.nextInt(16);
+            imageViews[moleLocation].setImageDrawable(moleImage);
+            imageViews[moleLocation].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //TODO This is where I want to increment the 0
+                    Log.w("fatal", "I clicked on imageView");
+                }
+            });
+            handler.postDelayed(moleHopper, 5000);
+        }
     }
 
 }
